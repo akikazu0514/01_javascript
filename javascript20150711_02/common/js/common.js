e764f3a9a0aebd2545c565485a56dbb79478ff38
岡田 晃和 <kouwa0514@gmail.com>
2015-07-14 08:47:44 +0900

    修正

--- javascript20150711_02/common/js/common.js
+++ javascript20150711_02/common/js/common.js
@@ -3,32 +3,33 @@ $(function(){
 	var
 	$gNav = $('nav li a'),
 	$pageNumber = $('nav li a').length,//ナビゲーションの数
-	pageCount = 0,//ページの初期設定
+	pageCount = 1,//ページの初期設定
 	oldPage = 0,//古いページの初期設定
-	timer = 1000,//ページ切り替えのスピード
+	timer = 3000,//ページ切り替えのスピード
 	timerStop;//clearTimeoutのグローバル変数
 	
 	//ページの切り替え
 	function pageCangeFunc(newPage,overTimeStop){
 		
-		var page = pageCount++,
-			newPage = page % $pageNumber;//余り計算で数字のループ処理
-			
-		$('section div').not('#scene' + oldPage).removeClass('pageIn');
-		console.log(oldPage);
-		$('#scene' + newPage).addClass('pageIn');
-		oldPage = newPage;
-		
 		//マウスオーバーの時に時間をストップ
 		if(overTimeStop) return;
 		timeFunc();
+		
+		var
+		page = pageCount++,
+		newPage = page % $pageNumber;//余り計算で数字のループ処理
+		
+		$('#scene' + oldPage).removeClass('pageIn');
+		$('#scene' + newPage).addClass('pageIn');
+		oldPage = newPage;
+		console.log("time" + newPage);	
 	}
 	
 	//時間の管理
-	function timeFunc(){
+	function timeFunc(timeTest){
 		timerStop = setTimeout(function() {
 			pageCangeFunc();
-		}, timer);
+		},timer);
 	}
 	
 	//発火
@@ -40,7 +41,13 @@ $(function(){
 			clearTimeout(timerStop);
 			var over = true;
 			pageCangeFunc(index,over);
-				 
+			
+			newPage = index;
+			$('#scene' + oldPage).removeClass('pageIn');
+			$('#scene' + newPage).addClass('pageIn');
+			oldPage = newPage;
+			
+			console.log("over" + newPage);	 
 		})
 		.mouseout(function(){
 			timeFunc();
